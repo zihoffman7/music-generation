f6bff5b2b04d6a859dffe7a1dd4dd18782879363
zih <74742492+galactaknife@users.noreply.github.com>
2021-07-01 19:17:46 -0700

    Update algorithm

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 import numpy as np
 import os, re
+from keras.utils import np_utils
 
 # Holds each note/chord corresponding to a number
 vocab = {}
 vectorized = {}
  # Max length of the training vector
-GENSIZE = 320
+GENSIZE = 500
 # Max vocabulary that will be learned
 VOCAB_THRESHOLD = 2000
 
@@ -22,42 +23,56 @@ def parse_abc(path, gensize, key="c", time_signature="4/4"):
                 return False
             # Remove unnecessary abc characters and cut model to not exceed max length
             try:
-                return list(filter(lambda a: a != " " and a != "\n", re.split("(\s|\/|\||\n)", "".join(content[[i for i, s in enumerate(content) if "K:" in s or "\n" == s][0] + 1:]).replace("\n", "").replace("\\", ""))))[:gensize]
+                return list(filter(lambda a: a != " " and a != "\n", re.split("(\s|\/|\||\n)", "".join(content[[i for i, s in enumerate(content) if "K:" in s or "%" in s or "C:" in s or "\n" == s][0] + 1:]).replace("\n", "").replace("\\", ""))))[:gensize]
             except IndexError:
                 return False
 
-def load_data(gensize=320, **kwargs):
+def load_data(gensize=320, vocab_threshold=5000, **kwargs):
+    global vocab
     # Will hold the processed train and test data
     data = []
     # Iterate through all abc files in song directory
     for file in os.listdir("songs"):
-        # Try to extract the valid abc
+        # Try to extract the valid abcd
         x = parse_abc(f"songs/{os.fsdecode(file)}", gensize, **kwargs)
         if not x:
             continue
-        # Create empty vector of length gensize
-        m = np.zeros([gensize], dtype=np.int32)
-        # Iterate through each value of abc
         for i in x:
-            # Exit process if too much vocab
-            if len(vocab) >= VOCAB_THRESHOLD:
-                return data[:round(len(data) * 0.8)], data[round(len(data) * 0.8):]
-            # Add the current value to vocab if not there already
-            if not i in vocab:
-                vocab[i] = len(vocab.keys())
-                vectorized[i] = len(vocab.keys()) + 1
-            # Add the value's vectorized number to the data vector
-            if i in vectorized.keys():
-                m[x.index(i)] = int(vectorized[i])
-        # Add the song vector to the data
-        data.append(m)
-    # Split data into training (80%) and test (20%)
-    return data[:round(len(data) * 0.8)], data[round(len(data) * 0.8):]
+            if len(vocab) + len(x) >= vocab_threshold:
+                break
+            vocab = {**vocab, **dict((c, i) for i, c in enumerate(x))}
+        data.append("".join(x))
+
+    char_len = len("".join(data))
+    vocab_keys = sorted(list(set(vocab.keys())))
+
+    data_x = []
+    data_y = []
+
+    for i in range(0, char_len - gensize):
+        if (i + gensize >= len(vocab_keys)):
+            break
+        inp = vocab_keys[i:i + gensize]
+        out = vocab_keys[i + gensize]
+        data_x.append([vocab[char] for char in inp])
+        data_y.append(vocab[out])
+    # training data is now [patterns, size, 1]
+    training_x = np.reshape(data_x, (len(data_x), gensize, 1)) / float(len(vocab.keys()))
+    training_y = np.eye(len(vocab))[data_y]
+
+    print(training_x)
+    print(training_x.shape)
+    print("-------")
+    print(training_y)
+    print(training_y.shape)
+    return training_x, training_y
 
 # Split data into train and test
-train, test = load_data(GENSIZE)
+train_x, train_y = load_data(GENSIZE, VOCAB_THRESHOLD)
+
+
 # Data summary
-print(f"Length of train data: {len(train)} songs\nLength of test data: {len(test)} songs\nTotal vocab length: {len(vocab)}")
+print(f"Length of training data: {len(train_x)}")
 
 # from music21 import converter
 # s = converter.parse('test.abc')
