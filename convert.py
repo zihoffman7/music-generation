05ac933c90ec2a25fb29a3e96a1105ca7ea25a49
zih <74742492+galactaknife@users.noreply.github.com>
2021-07-07 12:22:15 -0700

    Added support for midi

--- convert.py
+++ convert.py
@@ -1,12 +1,13 @@
 from music21 import converter, interval, pitch
 import os
-    
+
 def mid_to_abc(dir):
     for file in os.listdir(dir):
         path = f"{dir}/{file}"
+        fname = path.split("/")[-1].split(".")[0]
         s = converter.parse(path).makeNotation()
         s = s.transpose(interval.Interval(s.analyze("key").tonic, pitch.Pitch("C")))
-        s.write("xml", fp=f"{path.split('/')[-1].split('.')[0]}.xml")
-        os.system(f"python xml2abc.py {path.split('/')[-1].split('.')[0]}.xml -o abc")
-        os.remove(f"{path.split('/')[-1].split('.')[0]}.xml")
-        s = converter.parse(f"abc/{path.split('/')[-1].split('.')[0]}.abc")
+        s.write("xml", fp=f"{fname}.xml")
+        os.system(f"python xml2abc.py {fname}.xml -o abc")
+        os.remove(f"{fname}.xml")
+    print("Done!")
