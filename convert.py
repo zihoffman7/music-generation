cc8d2fbcd3fcdc27002abc6f17b689ee1459699e
zih <74742492+galactaknife@users.noreply.github.com>
2021-07-07 14:48:39 -0700

    Fixed bug when adding vocab

--- convert.py
+++ convert.py
@@ -3,11 +3,24 @@ import os
 
 def mid_to_abc(dir):
     for file in os.listdir(dir):
+        print(file)
+        # Ensure the file is a midi file
+        if not file.split(".")[-1].lower() == "mid" and not file.split(".")[-1].lower() == "midi":
+            continue
         path = f"{dir}/{file}"
+        print("loading")
         fname = path.split("/")[-1].split(".")[0]
+        # Load midi file
         s = converter.parse(path).makeNotation()
+        print("loaded")
+        # Attempt to transpose the file to C major
         s = s.transpose(interval.Interval(s.analyze("key").tonic, pitch.Pitch("C")))
+        # Convert music21 stream object to MusicXML
         s.write("xml", fp=f"{fname}.xml")
+        # Convert MusicXML to abc using xml2abc
+        print("converting")
         os.system(f"python xml2abc.py {fname}.xml -o abc")
+        # Remove the temporary MusicXML file
         os.remove(f"{fname}.xml")
     print("Done!")
+mid_to_abc("midi")
--- data.py
+++ data.py
@@ -1,33 +1,31 @@
 import numpy as np
-import os, re
+import os, re, random
 
 # Holds an element corresponding to a number
 vocab = {}
- # Max length of the training vector
-GEN_SIZE = 5000
 # Length of patterns to be detected
 SEQ_LEN = 10
-# Max vocabulary that will be learned
+# Max vocabulary that can be learned
 VOCAB_THRESHOLD = 2000
 
-def parse_abc(path, gen_size, key="c", time_signature="4/4"):
+def parse_abc(path, key="c", time_signature="4/4"):
     if path.endswith(".abc"):
         with open(path) as f:
             content = f.readlines()
             # Check if the key is in C major
-            if not f"k:{key.lower()}" in "".join(content).lower() and not f"k: {key.lower()}" in "".join(content).lower():
+            if not f"k:{key.lower()}" in "".join(content).lower() and not f"k: {key.lower()}" in "".join(content).lower() and not "k:none" in "".join(content).lower():
                 return False
             # Check if the time signature is 4/4
             if not f"m:{time_signature.lower()}" in "".join(content).lower() and not f"m: {time_signature.lower()}" in "".join(content).lower():
                 return False
             # Remove unnecessary abc characters and cut model to not exceed max length
             try:
-                return list(filter(lambda a: a != " " and a != "\n" and a != "/" and a != "" and a[0] != "%", re.split("(\s|\/|\||\n)", "".join(content[[i for i, s in enumerate(content) if "v:1\n" in s.lower() or "v:1 " in s.lower()][1] + 1:[i for i, s in enumerate(content) if "v:2\n" in s.lower() or "v:2 " in s.lower()][1]]).replace("\n", "").replace("\\", ""))))[:gen_size]
+                return list(filter(lambda a: a != " " and a != "\n" and a != "/" and a != "" and a[0] != "%", re.split("(\s|\/|\||\n)", "".join(content[[i for i, s in enumerate(content) if "v:1\n" in s.lower() or "v:1 " in s.lower()][1] + 1:[i for i, s in enumerate(content) if "v:2\n" in s.lower() or "v:2 " in s.lower()][1]]).replace("\n", "").replace("\\", ""))))
             except IndexError:
-                print("oof")
                 return False
 
-def split_data(data, gen_size, seq_len):
+def split_data(data, seq_len):
+    global vocab
     char_len = sum([len(x) for x in data])
 
     data_x = []
@@ -51,40 +49,45 @@ def split_data(data, gen_size, seq_len):
                 except KeyError:
                     continue
                 continue
-    return data_x, data_y
 
-def load_data(gen_size=320, vocab_threshold=5000, seq_len=50, **kwargs):
+    # train x = [number of patterns, size of pattern, 1]
+    data_x = np.reshape(data_x, (len(data_x), seq_len, 1))
+    # Resize train x to be between zero and 1
+    data_x = data_x / float(len(vocab))
+    # Designate each column for a vocab term.
+    # A '1' in column represents its corresponding vocab term
+    d_y = np.eye(len(data_y), len(vocab))[data_y]
+    return data_x, d_y
+
+def shuffle_data(x, y):
+    # Shuffle train and test data
+    c = list(zip(x, y))
+    random.shuffle(c)
+    return zip(*c)
+
+def load_data(vocab_threshold=5000, seq_len=50, **kwargs):
     global vocab
     # Will hold the processed train and test data
     data = []
     # Iterate through all abc files in song directory
     for file in os.listdir("abc"):
         # Try to extract the valid abcd
-        s = parse_abc(f"abc/{os.fsdecode(file)}", gen_size, **kwargs)
+        s = parse_abc(f"abc/{os.fsdecode(file)}", **kwargs)
         if not s:
             continue
         for i in s:
             if len(vocab) + len(s) >= vocab_threshold:
                 break
-            vocab = {**vocab, **dict((c, i) for i, c in enumerate(s) if len(c))}
+            vocab = {**vocab, **dict((c, len(vocab)) for c in s if len(c) and c not in vocab.keys())}
         data.append(list(filter(lambda a: len(a), s)))
 
-    x, y = split_data(data, gen_size, seq_len)
-    # train x = [number of patterns, size of pattern, 1]
-    training_x = np.reshape(x, (len(x), seq_len, 1))
-
-    # Resize train x to be between zero and 1
-    training_x = training_x / float(len(vocab))
-    # Designate each column for a vocab term.
-    # A '1' in column represents its corresponding vocab term
-    training_y = np.eye(len(y), len(vocab))[y]
-
-    return training_x, training_y
+    # Get x and y data
+    return [np.array(i) for i in shuffle_data(*split_data(data, seq_len))]
 
 # Split data into train and test
-train_x, train_y = load_data(GEN_SIZE, VOCAB_THRESHOLD, SEQ_LEN)
+x, y = load_data(VOCAB_THRESHOLD, SEQ_LEN)
 
 # Data summary
-print(f"Train x shape: {train_x.shape}")
-print(f"Train y shape: {train_y.shape}")
-print(f"Length of training data: {len(train_x)}")
+print(f"x data shape: {x.shape}")
+print(f"y data shape: {y.shape}")
+print(f"Length of data: {len(x)}")
